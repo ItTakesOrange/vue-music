7dbb440fb9ba2e2377d57189865d3119d460aea2
Fliwat <574562727@qq.com>
2019-07-01 21:43:34 +0800

    feat(chapter4.3): jsonp应用之推荐页面数据获取

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,14 +1,57 @@
 <template>
-  <div>
-    推荐
+  <div class="recommend">
+    <div class="recommend-content">
+      <div class="slider-wrapper">
+
+      </div>
+      <div class="recommend-list">
+        <h1 class="list-title">热门歌单推荐</h1>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { getRecommend } from 'api/recommend'
+import { ERR_OK } from 'api/config'
+
 export default {
-  name: 'Recommend'
+  name: 'Recommend',
+  created () {
+    this._getRecommend()
+  },
+  methods: {
+    _getRecommend () {
+      getRecommend().then(res => {
+        if (res.code === ERR_OK) {
+          console.log(res.data.slider)
+        }
+      })
+    }
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
+@import '~common/stylus/variable'
+
+.recommend
+  position: fixed
+  width: 100%
+  top: 88px
+  bottom: 0
+  .recommend-content
+    height: 100%
+    overflow: hidden
+    .slide-wrapper
+      position: relative
+      width: 100%
+      overflow hidden
+    .recommend-list
+      .list-title
+        height: 65px;
+        line-height: 65px
+        text-align: center
+        font-size: $font-size-medium
+        color: $color-theme
 </style>
--- vue.config.js
+++ vue.config.js
@@ -9,5 +9,6 @@ module.exports = {
     config.resolve.alias
       .set('common', resolve('src/common'))
       .set('components', resolve('src/components'))
+      .set('api', resolve('src/api'))
   }
 }
