1b5cd072d0206a87de948599c6986ccae76f75af
Fliwat <574562727@qq.com>
2019-07-04 22:24:37 +0800

    feat(chapter4.9-4.10): 显示歌单列表 & 封装scroll组件

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,23 +1,37 @@
 <template>
   <div class="recommend">
-    <div class="recommend-content">
-      <div v-if="recommends.length" class="slider-wrapper">
-        <slider>
-          <div v-for="(item, index) in recommends" :key="index">
-            <a :href="item.linkUrl">
-              <img :src="item.picUrl">
-            </a>
-          </div>
-        </slider>
+    <scroll class="recommend-content" :data="discList">
+      <div>
+        <div v-if="recommends.length" class="slider-wrapper">
+          <slider>
+            <div v-for="(item, index) in recommends" :key="index">
+              <a :href="item.linkUrl">
+                <img :src="item.picUrl">
+              </a>
+            </div>
+          </slider>
+        </div>
+        <div class="recommend-list">
+          <h1 class="list-title">热门歌单推荐</h1>
+          <ul>
+            <li class="item" v-for="(item, index) in discList" :key="index">
+              <div class="icon">
+                <img width="60" height="60" :src="item.imgurl">
+              </div>
+              <div class="text">
+                <h2 class="name" v-html="item.creator.name"></h2>
+                <p class="desc" v-html="item.dissname"></p>
+              </div>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div class="recommend-list">
-        <h1 class="list-title">热门歌单推荐</h1>
-      </div>
-    </div>
+    </scroll>
   </div>
 </template>
 
 <script>
+import Scroll from 'base/scroll/scroll'
 import Slider from 'base/slider/slider'
 import { getRecommend, getDiscList } from 'api/recommend'
 import { ERR_OK } from 'api/config'
@@ -26,7 +40,8 @@ export default {
   name: 'recommend',
   data () {
     return {
-      recommends: []
+      recommends: [],
+      discList: []
     }
   },
   created () {
@@ -44,13 +59,14 @@ export default {
     _getDiscList () {
       getDiscList().then(res => {
         if (res.code === ERR_OK) {
-          console.log(res.data.list)
+          this.discList = res.data.list
         }
       })
     }
   },
   components: {
-    Slider
+    Slider,
+    Scroll
   }
 }
 </script>
@@ -77,4 +93,26 @@ export default {
         text-align: center
         font-size: $font-size-medium
         color: $color-theme
+      .item
+        display: flex
+        box-sizing: border-box
+        align-items: center
+        padding: 0 20px 20px 20px
+        .icon
+          flex: 0 0 60px
+          width: 60px
+          padding-right: 20px
+        .text
+          display: flex
+          flex-direction: column
+          justify-content: center
+          flex: 1
+          line-height: 20px
+          overflow: hidden
+          font-size: $font-size-medium
+          .name
+            margin-bottom: 10px
+            color: $color-text
+          .desc
+            color: $color-text-d
 </style>
