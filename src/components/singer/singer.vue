01ae31b1d5f3d8582b833e8c9eacd982c366dfc7
Fliwat <574562727@qq.com>
2019-07-07 15:01:38 +0800

    feat(chapter5.1-5.3): 歌手页面数据获取 & 数据处理

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,14 +1,82 @@
 <template>
-  <div>
+  <div class="singer">
     歌手
   </div>
 </template>
 
 <script>
+import { getSingerList } from 'api/singer'
+import { ERR_OK } from 'api/config'
+import Singer from 'common/js/singer'
+
+const HOT_NAME = '热门'
+const HOT_SINGER_LEN = 10
+
 export default {
-  name: 'Singer'
+  name: 'Singer',
+  data () {
+    return {
+      singers: []
+    }
+  },
+  mounted () {
+    this._getSingerList()
+  },
+  methods: {
+    _getSingerList () {
+      getSingerList().then(res => {
+        if (res.code === ERR_OK) {
+          this.singers = res.data.list
+          console.log(this._normalizeSinger(this.singers))
+        }
+      })
+    },
+    _normalizeSinger (list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+
+      list.forEach((item, index) => {
+        if (index < HOT_SINGER_LEN) {
+          map.hot.items.push(new Singer(item.Fsinger_mid, item.Fsinger_name))
+        }
+        const key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push(new Singer(item.Fsinger_mid, item.Fsinger_name))
+      });
+
+      // 转化成有序数组
+      let hot = []
+      let ret = []
+      for (let key in map) {
+        let val = map[key]
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val)
+        } else if (val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+      ret.sort(function (a, b) {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      return hot.concat(ret)
+    }
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
+.singer
+  position: fixed
+  top: 88px
+  bottom: 0
+  width: 100%
 </style>
