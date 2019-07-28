a6af0be9dccf18fcaf25fdf4581f94963bd6a697
Fliwat <z55193z@qq.com>
2019-07-28 20:25:02 +0800

    feat(chapter10.1-10.3): 搜索页面search-box组件 & 热门搜索数据获取及应用

--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,14 +1,82 @@
 <template>
-  <div>
-    搜索
+  <div class="search">
+    <div class="search-box-wrapper">
+      <search-box ref="searchBox"></search-box>
+    </div>
+    <div class="shortcut-wrapper">
+      <div class="shortcut">
+        <div class="hot-key">
+          <h1 class="title">热门搜索</h1>
+          <ul>
+            <li @click="addQuery(item.k)" v-for="(item, index) in hotKey" class="item" :key="index">
+              <span>{{item.k}}</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'Search'
-}
+  import SearchBox from 'base/search-box/search-box'
+  import { getHotKey } from 'api/search'
+  import { ERR_OK } from 'api/config'
+
+  export default {
+    name: 'Search',
+    data () {
+      return {
+        hotKey: []
+      }
+    },
+    created () {
+      this._getHotKey()
+    },
+    methods: {
+      addQuery (query) {
+        this.$refs.searchBox.setQuery(query)
+      },
+      _getHotKey () {
+        getHotKey().then(res => {
+          if (res.code === ERR_OK) {
+            this.hotKey = res.data.hotkey.slice(0, 10)
+          }
+        })
+      }
+    },
+    components: {
+      SearchBox
+    }
+  }
 </script>
 
 <style lang="stylus" scoped>
+  @import "~common/stylus/variable"
+
+  .search
+    .search-box-wrapper
+      margin: 20px
+    .shortcut-wrapper
+      position: fixed
+      top: 178px
+      bottom: 0
+      width: 100%
+      .shortcut
+        height: 100%
+        overflow: hidden
+        .hot-key
+          margin: 0 20px 20px 20px
+          .title
+            margin-bottom: 20px
+            font-size: $font-size-medium
+            color: $color-text-l
+          .item
+            display: inline-block
+            padding: 5px 10px
+            margin: 0 20px 10px 0
+            border-radius: 6px
+            background: $color-highlight-background
+            font-size: $font-size-medium
+            color: $color-text-d
 </style>
